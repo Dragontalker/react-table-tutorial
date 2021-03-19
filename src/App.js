cb994031a2f39dc645fb12b717cc12821f237ddd
Richard <richard.yang.tong@gmail.com>
2021-03-19 13:42:42 -0400

    Fixed the code styles in app.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import { BasicTable } from './components/BasicTable';
 import MOCK_DATA from './components/MOCK_DATA.json';
 import { COLUMNS } from './components/columns';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <BasicTable columns={COLUMNS} data={MOCK_DATA}/>
