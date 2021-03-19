dcdd5e4869309c474af5d772944898814471bd9a
Richard <richard.yang.tong@gmail.com>
2021-03-19 17:09:53 -0400

    Imported useGlobalFilter from react-table.

--- src/components/FilteringTable.js
+++ src/components/FilteringTable.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
+import { useTable, useGlobalFilter } from 'react-table';
 import './table.css';
 
 export const FilteringTable = (props) => {
