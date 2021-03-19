2e3ac22e5b7a5c4facc194aebe18e295e7e5c532
Richard <richard.yang.tong@gmail.com>
2021-03-19 14:16:02 -0400

    Extract footerGroups from table instance.

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -16,6 +16,7 @@ export const BasicTable = (props) => {
         getTableProps, 
         getTableBodyProps,
         headerGroups,
+        footerGroups,
         rows,
         prepareRow
     } = tableInstance;
