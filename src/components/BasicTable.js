dbc0f5f5654bb45683dbba795cd98a7022611130
Richard <richard.yang.tong@gmail.com>
2021-03-19 11:41:13 -0400

    Imported useMemo hook.

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 import MOCK_DATA from './MOCK_DATA.json';
 import { COLUMNS } from './columns';
 
 export const BasicTable = () => {
+
+    useTable({
+        columns: COLUMNS,
+        data: MOCK_DATA
+    })
+
     return (
         <div>
 
