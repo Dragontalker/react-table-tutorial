56083c0551f3855cb1e6205e0fe647b387624407
Richard <richard.yang.tong@gmail.com>
2021-03-19 11:53:38 -0400

    Hooked data to memo so it does not get rerendered everytime.

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -5,6 +5,9 @@ import { COLUMNS } from './columns';
 
 export const BasicTable = () => {
 
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => MOCK_DATA, []);
+
     useTable({
         columns: COLUMNS,
         data: MOCK_DATA
