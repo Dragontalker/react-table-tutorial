eebc88a8f773daa5664369445206043afc47137d
Richard <richard.yang.tong@gmail.com>
2021-03-19 14:42:29 -0400

    Added the grouped columns.

--- src/components/columns.js
+++ src/components/columns.js
@@ -39,7 +39,19 @@ export const GROUPED_COLUMNS = [
     },
     {
         Header: 'Name',
-        Footer: 'Name'
+        Footer: 'Name',
+        columns: [
+            {
+                Header: 'First Name',
+                Footer: 'First Name',
+                accessor: 'first_name'
+            },
+            {
+                Header: 'Last Name',
+                Footer: 'Last Name',
+                accessor: 'last_name'
+            }
+        ]
     },
     {
 
