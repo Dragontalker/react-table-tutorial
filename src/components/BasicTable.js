d920b3fb0e837c49512b4ac53da0e99dcfb57a56
Richard <richard.yang.tong@gmail.com>
2021-03-19 13:20:00 -0400

    Improved the identation.

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -25,33 +25,24 @@ export const BasicTable = () => {
     return (
         <table {...getTableProps()}>
             <thead>
-                {
-                    headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {
-                                headerGroup.headers.map(column => (
-                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                                ))
-                            }
-                        </tr>
-                    ))
-                }
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}   
+                    </tr>
+                ))}
             </thead>
             <tbody {...getTableBodyProps()}>
-                {
-                    rows.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr>
-                                {
-                                    row.cells.map(cell => {
-                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    })
-                                }
-                            </tr>
-                        )
-                    })
-                }
+                {rows.map(row => {
+                    prepareRow(row)
+                    return (
+                        <tr> {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
     )
